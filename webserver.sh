651c5dc615e423bcf100d161a87a42cffc7b7928
Andrij Nychyporuk <pro.pokraska@gmail.com>
2022-11-06 23:45:20 +0200

    2

--- webserver.sh
+++ webserver.sh
@@ -23,7 +23,12 @@ echo '<title>Test</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<h1>Andriy Nychyporuk</h1>' >> index.html
+echo '<h1>And Nychyporuk</h1>' >> index.html
 echo '<h3></h3>' >> index.html
 echo '</body>' >> index.html
 echo '</html>' >> index.html
+
+cd /home/andrij/Bash/GL/GL2022/GL2022
+git add webserver.sh
+git commit -m "2"
+git push
